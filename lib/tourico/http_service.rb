a93b022da2e47503046da8ac9bde1c4b07a3e579
Vitali Margolin <vitali.m86@gmail.com>
2014-01-15 17:46:56 +0200

    added some info about request

--- lib/tourico/http_service.rb
+++ lib/tourico/http_service.rb
@@ -5,7 +5,7 @@ module Tourico
 
     class << self
        def make_request(action, args, options = {})
-
+         puts 'Making request to Tourico'
          client = Savon::Client.new(Tourico.hotel_service_link)
 
          #client.log_level = :error
@@ -29,7 +29,7 @@ module Tourico
            soap.body = args
 
          end
-
+         puts 'Finished request for Tourico'
          if response.success?
              response.to_hash
          else
@@ -38,7 +38,7 @@ module Tourico
        end
 
        def make_request_reservation_service(action, args, options = {})
-
+         puts 'Making request to Tourico'
          client = Savon::Client.new(Tourico.hotel_service_link)
 
          response = client.request :hot, action do
@@ -60,7 +60,7 @@ module Tourico
            soap.body = args
 
          end
-
+         puts 'Finished request for Tourico'
          response.to_hash
 
        end
