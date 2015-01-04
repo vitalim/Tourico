58b0d8a66009083ae9a606318036e42a93f0cf3e
shanys8 <shany@roomertravel.com>
2015-01-04 16:39:49 +0200

    added timeout option for http requests

--- lib/tourico.rb
+++ lib/tourico.rb
@@ -4,7 +4,7 @@ require 'tourico/http_service'
 
 module Tourico
   class << self
-    attr_accessor :login_name, :password, :culture, :hotels_service_version, :reservations_service_version, :location_service_version,:hotel_service_link,:reservation_service_link,:sandbox, :show_logs, :proxy_url, :digest_auth
+    attr_accessor :login_name, :password, :culture, :hotels_service_version, :reservations_service_version, :location_service_version,:hotel_service_link,:reservation_service_link,:sandbox, :show_logs, :proxy_url, :digest_auth, :open_timeout, :read_timeout
 
     # initializer with all the configuration values
     def setup
@@ -12,6 +12,8 @@ module Tourico
       self.hotel_service_link = 'http://demo-hotelws.touricoholidays.com/HotelFlow.svc?wsdl'
       self.reservation_service_link = 'http://demo-wsnew.touricoholidays.com/reservationsservice.asmx?wsdl'
       self.show_logs = false
+      self.open_timeout = 1
+      self.read_timeout = 10
       yield self
 
       # Savon.configure do|config|
--- lib/tourico/http_service.rb
+++ lib/tourico/http_service.rb
@@ -15,16 +15,22 @@ module Tourico
           end
           log Tourico.show_logs
           wsdl Tourico.hotel_service_link
+
+          if HTTPService.apply_timeout?(action)
+            open_timeout Tourico.open_timeout
+            read_timeout Tourico.read_timeout
+          end
+
           soap_header  'aut:AuthenticationHeader' => {
               'aut:LoginName' => Tourico.login_name,
-              'aut:Password' => Tourico.password,
-              'aut:Culture' => Tourico.culture,
-              'aut:Version' => Tourico.hotels_service_version
+              'aut:Password'  => Tourico.password,
+              'aut:Culture'   => Tourico.culture,
+              'aut:Version'   => Tourico.hotels_service_version
           }
           namespaces(
-              'xmlns:env' => 'http://schemas.xmlsoap.org/soap/envelope/',
-              'xmlns:aut' => 'http://schemas.tourico.com/webservices/authentication',
-              'xmlns:hot' => 'http://tourico.com/webservices/hotelv3',
+              'xmlns:env'  => 'http://schemas.xmlsoap.org/soap/envelope/',
+              'xmlns:aut'  => 'http://schemas.tourico.com/webservices/authentication',
+              'xmlns:hot'  => 'http://tourico.com/webservices/hotelv3',
               'xmlns:wsdl' => 'http://tourico.com/webservices/hotelv3',
               'xmlns:hot1' => 'http://schemas.tourico.com/webservices/hotelv3')
         end
@@ -51,14 +57,14 @@ module Tourico
           soap_header  'web:LoginHeader' => {
 		          'trav:username' => Tourico.login_name,
 		          'trav:password' => Tourico.password,
-		          'trav:culture' => Tourico.culture,
-		          'trav:version' => Tourico.reservations_service_version
+		          'trav:culture'  => Tourico.culture,
+		          'trav:version'  => Tourico.reservations_service_version
           }
           namespaces(
-		          'xmlns:env' => 'http://schemas.xmlsoap.org/soap/envelope/',
+		          'xmlns:env'  => 'http://schemas.xmlsoap.org/soap/envelope/',
 		          'xmlns:web'  => 'http://tourico.com/webservices/',
 		          'xmlns:hot'  => 'http://tourico.com/webservices/',
-		          'xmlns:wsdl'  => 'http://tourico.com/webservices/',
+		          'xmlns:wsdl' => 'http://tourico.com/webservices/',
 		          'xmlns:trav' => 'http://tourico.com/travelservices/')
         end
 
@@ -68,6 +74,12 @@ module Tourico
         response.to_hash
 
       end
+
+      #apply api call timeout except for while checkout
+      def apply_timeout?(action)
+        (action.to_s == 'book_hotel_v3') ? false : true
+      end
+
     end
   end
 end
